926afbca3d16b3c32e7650111b13581144113218
Aliaksei <alexx333by@protonmail.com>
2019-12-01 20:54:53 +0300

    Keil project base added

---
+++ Inc/main.h
@@ -0,0 +1,95 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f1xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define SIG_FREQ_Pin GPIO_PIN_1
+#define SIG_FREQ_GPIO_Port GPIOA
+#define SIG_Pin GPIO_PIN_2
+#define SIG_GPIO_Port GPIOA
+#define DISPLAY_CMD_Pin GPIO_PIN_6
+#define DISPLAY_CMD_GPIO_Port GPIOA
+#define DISPLAY_SYNC_Pin GPIO_PIN_7
+#define DISPLAY_SYNC_GPIO_Port GPIOA
+#define DISPLAY_DATA_0_Pin GPIO_PIN_8
+#define DISPLAY_DATA_0_GPIO_Port GPIOA
+#define DISPLAY_DATA_1_Pin GPIO_PIN_9
+#define DISPLAY_DATA_1_GPIO_Port GPIOA
+#define DISPLAY_DATA_2_Pin GPIO_PIN_10
+#define DISPLAY_DATA_2_GPIO_Port GPIOA
+#define DISPLAY_DATA_3_Pin GPIO_PIN_11
+#define DISPLAY_DATA_3_GPIO_Port GPIOA
+#define DISPLAY_DATA_4_Pin GPIO_PIN_12
+#define DISPLAY_DATA_4_GPIO_Port GPIOA
+#define DISPLAY_DATA_5_Pin GPIO_PIN_13
+#define DISPLAY_DATA_5_GPIO_Port GPIOA
+#define DISPLAY_DATA_6_Pin GPIO_PIN_14
+#define DISPLAY_DATA_6_GPIO_Port GPIOA
+#define DISPLAY_DATA_7_Pin GPIO_PIN_15
+#define DISPLAY_DATA_7_GPIO_Port GPIOA
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
